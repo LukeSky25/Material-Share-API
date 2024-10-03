eb351b838f10dbb29942c634864c3d2f448c0732
LukeSky25 <lucasskywalkerodrigues@gmail.com>
2024-10-02 22:14:17 -0300

    Eslist, Nodemon, Sucrase

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "semi": "error",
+      "class-methods-use-this": "off"
+    }
+  }
+];
